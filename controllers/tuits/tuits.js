829bcfb17e3d73b7523f7fe5512590a6f5f3a829
YIMING GE <yiming@MacBook-Air.local>
2023-03-25 00:47:23 -0700

    change id to string

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,24 +1,31 @@
 export default [
-  {
-    "_id": "123", "topic": "Space", "username": "SpaceX",
-    "handle": "@spacex", "time": "2h", "image": "spacex.png",
-    "title": "SpaceX's Mission",
-    "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-    "liked": true, "likes": 2345, "replies": 123, "retuits": 432
+  { "_id": "234", "topic": "Space",   "userName": "Tesla",
+    "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+    "time": "2h",   "image": "TSLA.jpeg",
+    "liked": false,
+    "replies": 1233,
+    "retuits": 4322,
+    "likes": 9345,
+    "handle": "@tesla",
+    "tuit": "To accelerate the advent of sustainable transport and electric technology."
   },
-  {
-    "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-    "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
-    "title": "The Boring Company fixing traffic",
-    "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-    "liked": true, "likes": 2345, "replies": 321, "retuits": 432
-  },
-  {
-    "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-    "handle": "@neurolink", "time": "3h", "image": "neurolink.jpg",
-    "title": "Neurolink fixes paralisis and blindness",
-    "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
-    "liked": true, "likes": 3456, "replies": 345, "retuits": 654
-  },
+  { "_id": "345", "topic": "Energy",  "userName": "Sasuke",
+    "title": "Tesla FSD finally makes driving obsolete. After years of delay, Tesla Full Self Driving can drive you to work while you work, play, or sleep",
+    "time": "4d",   "image": "sasuke.jpg",
+    "liked": true,
+    "replies": 231,
+    "retuits": 321,
+    "likes": 7345,
+    "handle": "@sasuke",
+    "tuit": "Cool..."},
+  { "_id": "123", "topic": "Science",  "userName": "NewYorkTimes",
+    "title": "Time To Sleep",
+    "time": "1h",   "image": "nyt.png",
+    "liked": false,
+    "replies": 31,
+    "retuits": 21,
+    "likes": 345,
+    "handle": "@nyt",
+    "tuit": "Good Night."}
 ];
 
